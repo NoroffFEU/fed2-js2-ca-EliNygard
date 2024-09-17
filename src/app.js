86de167ca1592f8828774dfa8190c0420a08f98c
EliNygard <eli.nygard@gmail.com>
2024-09-17 13:16:45 +0200

    test login to see request

--- src/app.js
+++ src/app.js
@@ -1,5 +1,20 @@
-import "./css/style.css";
+// import "./css/style.css";
+import SocialAPI from "./js/api/index.js";
 
-import router from "./js/router";
+import router from "./js/router/index.js";
 
 await router(window.location.pathname);
+console.log("test");
+
+const api = new SocialAPI("https://v2.api.noroff.dev")
+
+const user = await api.auth.login({
+    email: "fake@email.com",
+    password: "fakePassword"
+})
+
+console.log(user);
+
+
+
+
