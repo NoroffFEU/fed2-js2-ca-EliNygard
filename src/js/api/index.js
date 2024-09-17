2a994bc7dbdb2513decf6f9f99079b40d2cab996
EliNygard <eli.nygard@gmail.com>
2024-09-17 15:12:34 +0200

    add fallback apiBase

--- src/js/api/index.js
+++ src/js/api/index.js
@@ -1,62 +1,70 @@
 export default class SocialAPI {
+  apiBase = "";
 
-    constructor(apiBase) {
-        this.apiBase = apiBase;
-    }
+  constructor(apiBase = "https://v2.api.noroff.dev") {
+    this.apiBase = apiBase;
+  }
 
-    get apiRegisterPath() {
-        return `${this.apiBase}/auth/register`
-    }
-    get apiLoginPath() {
-        return `${this.apiBase}/auth/login`
-    }
+  get apiRegisterPath() {
+    return `${this.apiBase}/auth/register`;
+  }
+  get apiLoginPath() {
+    return `${this.apiBase}/auth/login`;
+  }
 
+  auth = {
+    login: async ({ email, password }) => {
+      const body = JSON.stringify({ email, password });
 
-    auth = {
-        login: async ({ email, password }) => {
-            const body = JSON.stringify({ email, password})
-
-            const response = await fetch(this.apiLoginPath, {
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                method: "post",
-                body,
-            })
-
-            if (response.ok) {
-                const { data } = await response.json()
-                const { accessToken: token, ...user } = data;
-                localStorage.token = token;
-                localStorage.user = JSON.stringify(user)
-                return data;
-            }
-
-            throw new Error("Error")
+      const response = await fetch(this.apiLoginPath, {
+        headers: {
+          "Content-Type": "application/json",
         },
+        method: "post",
+        body,
+      });
 
-        register: async ({ name,
-            email,
-            password,
-            bio,
-            banner,
-            avatar, }) => {
-                const body = JSON.stringify({ name, email, password, bio, banner, avatar })
+      if (response.ok) {
+        const { data } = await response.json();
+        const { accessToken: token, ...user } = data;
+        localStorage.token = token;
+        localStorage.user = JSON.stringify(user);
+        return data;
+      }
 
-                const response = await fetch(this.apiRegisterPath, {
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    method: "post",
-                    body,
-                })
+      throw new Error("Error");
+    },
 
-                if (response.ok) {
-                    const { data } = await response.json()
-                    return data;
-                }
+    register: async ({ name, email, password, bio, banner, avatar }) => {
+      const body = JSON.stringify({
+        name,
+        email,
+        password,
+        bio,
+        banner,
+        avatar,
+      });
 
-                throw new Error("Something went wrong. Could not register account.")
-            }
-    }
+      const response = await fetch(this.apiRegisterPath, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "post",
+        body,
+      });
+
+      if (response.ok) {
+        const { data } = await response.json();
+        return data;
+      }
+
+      throw new Error("Something went wrong. Could not register account.");
+    },
+
+    logout: () => {
+        localStorage.removeItem("token")
+        localStorage.removeItem("user")
+        window.location.href = "/"
+    },
+  };
 }
