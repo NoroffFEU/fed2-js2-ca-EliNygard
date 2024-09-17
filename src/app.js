08b79780d6d86b2434d718b0a33928e0be81ae3a
EliNygard <eli.nygard@gmail.com>
2024-09-17 19:45:06 +0200

    adding and importing api instance from class SocialAPI

--- src/app.js
+++ src/app.js
@@ -1,13 +1,9 @@
 import "./css/style.css";
 
 import router from "./js/router/index.js";
-import SocialAPI from "./js/api/index.js"
+import api from "./js/api/instance.js"
 
 await router(window.location.pathname);
-
-const api = new SocialAPI()
-
-
 
 // Test logging in with a test user:
 // const user = {
