b26f58fd2708d9045085df3fdcb5debd7bca7c68
EliNygard <eli.nygard@gmail.com>
2024-09-17 15:12:49 +0200

    testing register with fake user

--- src/app.js
+++ src/app.js
@@ -1,15 +1,20 @@
-// import "./css/style.css";
-import SocialAPI from "./js/api/index.js";
+import "./css/style.css";
 
 import router from "./js/router/index.js";
+import SocialAPI from "./js/api/index.js"
 
 await router(window.location.pathname);
 
-const api = new SocialAPI("https://v2.api.noroff.dev")
+const api = new SocialAPI()
 
 
+const user = {
+    name: "fakeuser",
+    email: "fake@stud.noroff.no",
+    password: "fake1234"
+}
 
 
+await api.auth.register(user)
 
-
-
+// await api.auth.login({ email: user.email, password: user.password })
