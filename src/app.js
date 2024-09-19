106fe376a1bb8f493533454802ad0e4396397d84
EliNygard <eli.nygard@gmail.com>
2024-09-19 08:49:54 +0200

    comment out

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import "./css/style.css";
 
 import router from "./js/router/index.js";
-import api from "./js/api/instance.js";
+// import api from "./js/api/instance.js";
 
 await router(window.location.pathname);
 
