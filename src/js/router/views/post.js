03a0cbc90563fbb9bd3facc0ebe546a8b6c549a2
EliNygard <eli.nygard@gmail.com>
2024-09-19 14:28:11 +0200

    add todo comments, call viewPost

--- src/js/router/views/post.js
+++ src/js/router/views/post.js
@@ -1,1 +1,7 @@
-alert("Single Post Page");
+import { viewPost } from "../../ui/post/viewPost";
+
+console.log("Single Post Page");
+// have a file in ui > post > viewPost.js? Here I can have a function to get post and generate html and append
+//then on this page i call the function
+
+viewPost()
