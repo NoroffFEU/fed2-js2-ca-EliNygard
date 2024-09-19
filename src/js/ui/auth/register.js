373a2e94beef331a6cf80b2da208b8ca1ef6c3f1
EliNygard <eli.nygard@gmail.com>
2024-09-19 08:50:56 +0200

    adding onRegister functionality

--- src/js/ui/auth/register.js
+++ src/js/ui/auth/register.js
@@ -1,3 +1,20 @@
+import api from "../../api/instance.js";
+
 export async function onRegister(event) {
-    // get formData
+  event.preventDefault();
+  const form = event.target;
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData.entries());
+
+  //show loader
+
+  try {
+    await api.auth.register(data);
+  } catch (error) {
+    console.error(error);
+    alert(error);
+  } finally {
+    window.location.href = "/auth/login/"
+    //hide loader
+  }
 }
