eb01c591f3fbe566f01ec6a6995ea4e32b374a83
EliNygard <eli.nygard@gmail.com>
2024-09-19 11:31:55 +0200

    feat: login

--- src/js/ui/auth/login.js
+++ src/js/ui/auth/login.js
@@ -1,1 +1,19 @@
-export async function onLogin(event) {}
+import api from "../../api/instance.js"
+
+export async function onLogin(event) {
+    event.preventDefault()
+    const form = event.target;
+    const formData = new FormData(form)
+    const data = Object.fromEntries(formData.entries())
+
+    //show loader
+
+    try {
+        await api.auth.login(data)
+    } catch (error) {
+        console.error(error.messages);
+        alert(error)
+    } finally {
+        window.location.href= "/"
+    }
+}
