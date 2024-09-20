c3e851209457c2cdc70d64662b807e3850259e7c
EliNygard <eli.nygard@gmail.com>
2024-09-20 09:01:57 +0200

    remove comments

--- src/js/router/views/post.js
+++ src/js/router/views/post.js
@@ -1,7 +1,4 @@
 import { viewPost } from "../../ui/post/viewPost";
 
-console.log("Single Post Page");
-// have a file in ui > post > viewPost.js? Here I can have a function to get post and generate html and append
-//then on this page i call the function
 
 viewPost()
