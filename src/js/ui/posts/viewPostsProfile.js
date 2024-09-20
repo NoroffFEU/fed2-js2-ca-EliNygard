940c8c3f12a09984127814051530f7acea6955ee
EliNygard <eli.nygard@gmail.com>
2024-09-20 10:05:23 +0200

    prettier

--- src/js/ui/posts/viewPostsProfile.js
+++ src/js/ui/posts/viewPostsProfile.js
@@ -1,22 +1,21 @@
 import api from "../../api/instance.js";
 
 export async function viewPostsByProfile() {
-    try {
-        const posts = await api.profiles.posts()
-        console.log(posts);
+  try {
+    const posts = await api.profiles.posts();
+    console.log(posts);
 
-        const list = posts.map((post) => {
-            const li = document.createElement("li")
-            const a = document.createElement("a")
-            a.href = `/post/?id=${post.id}`
-            a.innerText = post.title
-            li.append(a)
-            return li
-        })
+    const list = posts.map((post) => {
+      const li = document.createElement("li");
+      const a = document.createElement("a");
+      a.href = `/post/?id=${post.id}`;
+      a.innerText = post.title;
+      li.append(a);
+      return li;
+    });
 
-        document.querySelector("#viewPostsProfile").append(...list)
-        
-    } catch (error) {
-        console.error(error);
-    }
+    document.querySelector("#viewPostsProfile").append(...list);
+  } catch (error) {
+    console.error(error);
+  }
 }
