4ce0935203b9f032e75b42479ac07ffbd0f8a14a
EliNygard <eli.nygard@gmail.com>
2024-09-20 08:41:34 +0200

    feat: viewPost html

--- src/js/ui/post/viewPost.js
+++ src/js/ui/post/viewPost.js
@@ -8,6 +8,26 @@ export async function viewPost() {
 
         const post = await api.post.read(id)
         console.log(post);
+
+        const section = document.createElement("section")
+        
+        const h2 = document.createElement("h2")
+        h2.textContent = post.title
+
+        const p = document.createElement("p")
+        p.textContent = post.body;
+
+        const btnEdit = document.createElement("button")
+        btnEdit.textContent = "Edit"
+
+        const btnDelete = document.createElement("button")
+        btnDelete.textContent = "Delete"
+
+        section.append(h2, p, btnEdit, btnDelete)
+
+        document.querySelector("body").appendChild(section)
+
+
     } catch(error) {
         console.error(error);
         
