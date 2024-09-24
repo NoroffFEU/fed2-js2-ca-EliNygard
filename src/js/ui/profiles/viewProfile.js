5853a83d172544f012a58f379619304c3aa2b959
EliNygard <eli.nygard@gmail.com>
2024-09-24 14:21:48 +0200

    start on view single profile page

---
+++ src/js/ui/profiles/viewProfile.js
@@ -0,0 +1,13 @@
+// import api from "../../api/instance.js";
+
+// export async function viewProfile(profile) {
+//     try {
+//         const profile = await api.profiles.readSingleProfile(profile);
+// console.log(profile);
+
+    
+//     } catch (error) {
+//         console.error();
+        
+//     }
+// }
