3db72da1e1432024424e0ae3babf8963474c6968
EliNygard <eli.nygard@gmail.com>
2024-09-26 21:25:06 +0200

    remove import

--- src/js/router/views/profile.js
+++ src/js/router/views/profile.js
@@ -1,6 +1,11 @@
 import { viewPostsByLoggedinProfile } from "../../ui/posts/viewPostsByLoggedinProfile";
 import { authGuard } from "../../utilities/authGuard";
+import api from "../../api/instance.js";
 
 authGuard();
 
 viewPostsByLoggedinProfile()
+
+
+
+
