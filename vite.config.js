bf1e45ff81a03827bc2cd59e71a6d17e6da92f9c
EliNygard <eli.nygard@gmail.com>
2024-10-10 14:41:27 +0200

    update base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   appType: "mpa",
-  base: "/fed2-js2-ca-EliNygard",
+  base: "/fed2-js2-ca-EliNygard/",
   build: {
     target: "esnext",
     rollupOptions: {
